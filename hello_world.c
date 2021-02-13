ec498d402204b4ef1c4c379370c62e26f29b7ccb
munnangi venkatakarthik <75118794+venkatakarthik29@users.noreply.github.com>
2021-02-13 11:40:22 +0530

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,1 +1,29 @@
 //Write the program to print "Hello World !"
+#include<stdio.h>
+float input()
+{
+float n;
+printf("enter number:");
+scanf("%f" &n);
+return n;
+}
+{
+float sum(float a,float b)
+{
+float sum;
+sum = a+b;
+return sum;
+}
+void output(float n)
+{
+printf("sum = %f" ,n);
+}
+int main()
+{
+float x,y,z;
+x = input();
+y = input();
+z = sum(x,y);
+output(z);
+return0;
+}
